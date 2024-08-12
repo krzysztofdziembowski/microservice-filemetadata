1c8d5b85b029588177657a5d26e95f85cfcf2d53
Krzysztof Dziembowski <studioaevia@gmail.com>
2024-08-12 08:13:33 +0200

    feat: handle post requests to /api/fileanalyse

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var cors = require('cors');
+var bodyparser = require('body-parser')
+var multer = require('multer')
 require('dotenv').config()
 
 var app = express();
+var upload = multer({dest: 'uploads/'})
 
 app.use(cors());
+app.use(bodyparser.urlencoded({extended: false}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -12,7 +16,21 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
 
+  const fileName = req.file.originalname;
+  const fileSize = req.file.size;
+  const fileType = req.file.mimetype;
+
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
